ce38ffe2e903ee38facf288403b2404154b9fc50
Jawaher Alkhunaifer <jawaheralkhonaifer@gmail.com>
2025-05-14 14:21:09 +0300

    Add files via upload

---
+++ python-package/employee_events/query_base.py
@@ -0,0 +1,70 @@
+# Import any dependencies needed to execute sql queries
+from sqlite3 import connect
+from .sql_execution import QueryMixin
+
+# Define a class called QueryBase
+# Use inheritance to add methods
+# for querying the employee_events database.
+class QueryBase(QueryMixin):
+
+    # Create a class attribute called `name`
+    # set the attribute to an empty string
+    name = ""
+
+    # Define a `names` method that receives
+    # no passed arguments
+    def names(self):
+        
+        # Return an empty list
+        return []
+
+
+    # Define an `event_counts` method
+    # that receives an `id` argument
+    # This method should return a pandas dataframe
+    def event_counts(self, id):
+
+        # QUERY 1
+        # Write an SQL query that groups by `event_date`
+        # and sums the number of positive and negative events
+        # Use f-string formatting to set the FROM {table}
+        # to the `name` class attribute
+        # Use f-string formatting to set the name
+        # of id columns used for joining
+        # order by the event_date column
+        sql = f"""
+        SELECT event_date,
+               SUM(positive_events) AS total_positive_events,
+               SUM(negative_events) AS total_negative_events
+        FROM {self.name}
+        JOIN employee_events
+            ON {self.name}.{self.name}_id = employee_events.{self.name}_id
+        WHERE {self.name}.{self.name}_id = {id}
+        GROUP BY event_date
+        ORDER BY event_date;
+        """
+        return super().pandas_query(sql)
+            
+    
+
+    # Define a `notes` method that receives an id argument
+    # This function should return a pandas dataframe
+    def notes(self, id):
+
+        # QUERY 2
+        # Write an SQL query that returns `note_date`, and `note`
+        # from the `notes` table
+        # Set the joined table names and id columns
+        # with f-string formatting
+        # so the query returns the notes
+        # for the table name in the `name` class attribute
+        sql = f"""
+        SELECT note_date, note
+        FROM notes 
+        JOIN {self.name}
+            ON {self.name}.{self.name}_id = notes.{self.name}_id
+        WHERE {self.name}.{self.name}_id = {id}
+        ORDER BY note_date;
+        """
+        return super().pandas_query(sql)
+
---
+++ python-package/employee_events/sql_execution.py
@@ -0,0 +1,58 @@
+from sqlite3 import connect
+from pathlib import Path
+from functools import wraps
+import pandas as pd
+import os
+# Using pathlib, create a `db_path` variable
+# that points to the absolute path for the `employee_events.db` file
+db_path = Path(__file__).parent / 'employee_events.db'
+
+
+# OPTION 1: MIXIN
+# Define a class called `QueryMixin`
+class QueryMixin:
+    
+    # Define a method named `pandas_query`
+    # that receives an sql query as a string
+    # and returns the query's result
+    # as a pandas dataframe
+    def pandas_query(self, sql_query:str):
+
+        with connect(db_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute(sql_query)
+            rows = cursor.fetchall()
+            columns = [desc[0] for desc in cursor.description]
+            return pd.DataFrame(rows, columns=columns)
+
+    # Define a method named `query`
+    # that receives an sql_query as a string
+    # and returns the query's result as
+    # a list of tuples. (You will need
+    # to use an sqlite3 cursor)
+    def query(self,sql_query:str):
+
+        with connect(db_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute(sql_query)
+            return cursor.fetchall()
+    
+
+    
+ # Leave this code unchanged
+def query(func):
+    """
+    Decorator that runs a standard sql execution
+    and returns a list of tuples
+    """
+
+    @wraps(func)
+    def run_query(*args, **kwargs):
+        query_string = func(*args, **kwargs)
+        connection = connect(db_path)
+        cursor = connection.cursor()
+        result = cursor.execute(query_string).fetchall()
+        connection.close()
+        return result
+    
+    return run_query
